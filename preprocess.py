c9d3be68a81d0bed5ed2417c2e3f5d73e6bc245b
Alexander <Alex@woodl.nl>
2016-04-15 16:29:58 +0200

    Changed sum to nansum to hopefully decrease number of nans

--- preprocess.py
+++ preprocess.py
@@ -6,10 +6,6 @@ import pickle
 
 d = pd.read_csv('dataset_mood_smartphone.csv', index_col=2, parse_dates=[2])
 
-# Sample hours
-# d0.ix[d0.index.indexer_between_time(datetime.time(9), datetime.time(12))]
-
-
 dp = d.pivot_table(index=[d.index, d.id], columns='variable', values='value')
 dp.reset_index(inplace=True)
 dp.index = dp.time
@@ -19,30 +15,30 @@ ids = dp.id.unique()
 
 resample_method_dictionary = {
     'activity': np.mean,
-    'appCat.builtin': np.sum,
-    'appCat.communication': np.sum,
-    'appCat.entertainment': np.sum,
-    'appCat.finance': np.sum,
-    'appCat.game': np.sum,
-    'appCat.office': np.sum,
-    'appCat.other': np.sum,
-    'appCat.social': np.sum,
-    'appCat.travel': np.sum,
-    'appCat.unknown': np.sum,
-    'appCat.utilities': np.sum,
-    'appCat.weather': np.sum,
-    'call': np.sum,
+    'appCat.builtin': np.nansum,
+    'appCat.communication': np.nansum,
+    'appCat.entertainment': np.nansum,
+    'appCat.finance': np.nansum,
+    'appCat.game': np.nansum,
+    'appCat.office': np.nansum,
+    'appCat.other': np.nansum,
+    'appCat.social': np.nansum,
+    'appCat.travel': np.nansum,
+    'appCat.unknown': np.nansum,
+    'appCat.utilities': np.nansum,
+    'appCat.weather': np.nansum,
+    'call': np.nansum,
     'circumplex.arousal': np.mean,
     'circumplex.valence': np.mean,
     'mood': np.mean,
-    'screen': np.sum,
-    'sms': np.sum,
+    'screen': np.nansum,
+    'sms': np.nansum,
 }
 
 data_per_id = {}
 for id in ids:
     current_d = dp.loc[dp.id == id].drop('id', 1)
-    current_d = current_d.resample('1D').agg(resample_method_dictionary)
+    current_d = current_d.resample('1D', ).agg(resample_method_dictionary)
     data = pd.DataFrame()
     for i in current_d[current_d.mood.notnull() == True].index:
         first_day = i - timedelta(days=5)
